b394069214a90ecddad0b9a359e959b94bc6d525
futakotome <983528987@qq.com>
2021-01-27 02:22:09 +0800

    modify

--- main/test.py
+++ main/test.py
@@ -1,13 +1,17 @@
 from wxpy import *
 
 bot = Bot()
-classGroup = bot.groups().search("外包2b猴子空调启动通知群⚽")[0]
 
 
-@bot.register(classGroup)
+# classGroup = ensure_one(bot.groups().search("外包2b猴子空调启动通知群⚽"))
+
+@bot.register(Group)
 def handle_message(msg):
-    print("发送人:" + msg.member)
-    return '收到消息:{}({})'.format(msg.text, msg.type)
-
+    if isinstance(msg.sender, Group):
+        class_group = msg.sender
+        print("是群聊")
+        if class_group.name == "外包2b猴子空调启动通知群⚽":
+            print("发送人:" + msg.member + " msg:" + msg)
+            return '收到消息:{}({})'.format(msg.text, msg.type)
 
 embed()
